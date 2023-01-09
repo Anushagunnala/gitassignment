a4d810427411a50997e30d54ee7db74c71591ca1
Harikrishna Gunnala <harikrishnagunnala833@gmail.com>
2023-01-09 23:52:58 +0530

    unique problem is done

---
+++ unique.js
@@ -0,0 +1,6 @@
+let arr=["anusha","hari","sinugoud","sudha","suma","anusha","hari","sinugoud","sudha","suma","anusha","hari","sinugoud","sudha","suma","shamili"];
+let object={};
+for(let i=0;i<arr.length;i++){
+  object[arr[i]]=1;
+}
+console.log(object);
